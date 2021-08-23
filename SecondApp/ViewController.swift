bda18beff63cb9a7bc87017f72feb777010386df
ohhn <gkdud644@naver.com>
2021-08-23 10:28:45 +0900

    initial commit

---
+++ SecondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by Admin on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
